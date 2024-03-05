c35851a8bb52116a3a8c5c6af5d38f9a2d26b371
FrancimarAlexandre <francimaralexandredt@gmail.com>
2024-03-05 09:07:28 -0300

    adicionado gênero do livro

--- books/models.py
+++ books/models.py
@@ -6,5 +6,5 @@ class Books(models.Model):
     titulo = models.CharField(max_length=255)
     descricao = models.CharField(max_length=255)
     autor = models.CharField(max_length = 255)
+    genero = models.CharField(max_length = 255)
     create_at = models.DateField(auto_now_add = True)
-    
