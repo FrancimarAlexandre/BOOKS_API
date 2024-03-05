fb4d66b0d42845529cd0c39858ce4979f798c900
FrancimarAlexandre <francimaralexandredt@gmail.com>
2024-03-05 08:43:27 -0300

    criando models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Books(models.Model):
+    titulo = models.CharField(max_length=255)
+    descricao = models.CharField(max_length=255)
+    autor = models.CharField(max_length = 255)
+    create_at = models.DateField(auto_now_add = True)
+    
